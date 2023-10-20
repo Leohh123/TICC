53629791e8e0d3174ebb4ccfc3361f8ca4b5cb60
Leohh <948750310@qq.com>
2023-10-20 13:49:37 +0000

    fix: create_env.sh with versions of dependencies

--- create_env.sh
+++ create_env.sh
@@ -1,3 +1,3 @@
-conda create -n py39ticc python=3.9 numpy scipy matplotlib scikit-learn pandas cvxpy -c conda-forge
+conda create -n py39ticc python=3.9.18 numpy=1.26.0 scipy=1.11.3 matplotlib=3.8.0 scikit-learn=1.3.1 pandas=2.1.1 cvxpy=1.4.1 -c conda-forge
 conda activate py39ticc
-python -m pip install snap-stanford
+python -m pip install snap-stanford==6.0.0
